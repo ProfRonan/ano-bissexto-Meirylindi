0da9aab01f04fca5f3d2adcb9d01861c9a7f9a60
Meirylindi <meirylindimlima@gmail.com>
2020-12-27 22:44:48 +0000

    Atividade concluída.

--- main.py
+++ main.py
@@ -3,9 +3,17 @@
 
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
-    # COLOQUE SEU CÓDIGO AQUI
-    # TODO
+    # ENTRADA DOS DADOS
+    
+    print('Digite o ano:')
+    ano = int(input('> '))
 
+    # MANIPULAÇÃO E EXIBIÇÃO DOS DADOS
+    
+    if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+      print(f'O ano {ano} é bissexto.')
+    else:
+      print(f'O ano {ano} não é bissexto.')
 
 if __name__ == '__main__':
     main()
